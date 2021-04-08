42fa7406dc007756c1668f8e031909edde2efa37
Karmit Personal <karmit199@gmail.com>
2021-04-08 19:50:58 -0400

    [feat] Added tests in ExistentialNormalFormTest and PositiveNormalFormTest

--- src/test/ExistentialNormalFormTest.java
+++ src/test/ExistentialNormalFormTest.java
@@ -8,70 +8,105 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import parser.CTLLexer;
 import parser.CTLParser;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotSame;
+import java.util.ArrayList;
 
 class ExistentialNormalFormTest {
 
-	private static Formula getFormula(String in) {
-		CharStream input = CharStreams.fromString(in);
-		CTLLexer lexer = new CTLLexer(input);
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		CTLParser parser = new CTLParser(tokens);
-		ParseTree tree = parser.root();
-		Generator generator = new Generator();
-		return generator.visit(tree);
-	}
+    public static final String p1 = "java.lang.error";
+    public static final String p2 = "java.lang.exception";
+    public static final String p3 = "java.lang.object";
 
-	@Test
-	void testFormulaUnchanged(){
+    private static Formula getFormula(String in) {
+        CharStream input = CharStreams.fromString(in);
+        CTLLexer lexer = new CTLLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CTLParser parser = new CTLParser(tokens);
+        ParseTree tree = parser.root();
+        Generator generator = new Generator();
+        return generator.visit(tree);
+    }
 
-		for(int i = 0; i < 10000; i++) {
-			Formula generated = RandomFormula.UntranslatableRandomFormula(10);
-			Formula translated = generated.existentialNormalForm();
-			assertEquals(translated.toString(), generated.toString());
-		}
+    @Test
+    void testFormulaUnchanged() {
 
-	}
+        for (int i = 0; i < 10000; i++) {
+            Formula generated = RandomFormula.UntranslatableRandomFormula(10);
+            Formula translated = generated.existentialNormalForm();
+            Assertions.assertEquals(translated.toString(), generated.toString());
+        }
 
-	@Test
-	void testFormulaChanged(){
-		for(int i = 0; i < 10000; i++) {
-			Formula generated = RandomFormula.translatablePNFRandomFormula(10);
-			Formula translated = generated.positiveNormalForm();
-			boolean isSame = translated.toString().equals(generated.toString());
-			assertEquals(false,isSame);
-		}
-	}
+    }
 
-
-	@Test
-	void testTranslatedForAllNext() {
-		String in = "AX(java.lang.Error)";
-		Formula formula = getFormula(in);
-		Formula g = ExistentialNormalForm.translate(formula);
-
-		System.out.println(g.toString());
-	}
-
-	@Test
-	void testTranslatedForAllUntil() {
-		String in = "AX(java.lang.Error)";
-		String in1 = "A (" + in + "U " + in +")";
-		Formula formula = getFormula(in1);
-		Formula g = ExistentialNormalForm.translate(formula);
-
-		Formula gg = g.existentialNormalForm();
-
-		System.out.println(g.toString());
-
-		System.out.println(gg.toString());
-	}
+    @Test
+    void testFormulaChanged() {
+        for (int i = 0; i < 10000; i++) {
+            Formula generated = RandomFormula.translatablePNFRandomFormula(10);
+            Formula translated = generated.positiveNormalForm();
+            boolean isSame = translated.toString().equals(generated.toString());
+            Assertions.assertFalse(isSame);
+        }
+    }
 
 
+    @Test
+    void testTranslatedForAllNext() {
+        String in = "AX(" + p1 + ")";
+        Formula formula = getFormula(in);
+        Formula g = ExistentialNormalForm.translate(formula);
+    }
+
+    @Test
+    void testTranslatedForAllUntil() {
+        String in = "AX(" + p1 + ")";
+        String in1 = "A (" + in + "U " + in + ")";
+        Formula formula = getFormula(in1);
+        Formula g = ExistentialNormalForm.translate(formula);
+
+        Formula gg = g.existentialNormalForm();
+    }
+
+    /**
+     * Method to test random samples that have ForAll Quantifier
+     */
+    @Test
+    void testRandomSamples() {
+        ArrayList<String> in = new ArrayList<String>();
+        in.add("AX(" + p1 + ")&&EX(" + p2 + ")");
+        in.add("EX(" + p1 + ")");
+        in.add("A(A(" + p1 + " U true) U A(true U " + p2 + "))");
+        in.add("(true && AX " + p1 + ")");
+        in.add("(true && AX(true && true))");
+        in.add("EX A(" + p2 + " U " + p3 + ")");
+        in.add("AX EX " + p3);
+        in.add("!AX " + p2);
+        in.add("EX A(" + p3 + " U " + p3 + ")");
+        in.add("!AX true");
+        in.add("A(A(" + p1 + " U " + p2 + ") U E(" + p2 + " U true))");
+
+        ArrayList<String> out = new ArrayList<String>();
+        out.add("(!(E(X(!(" + p1 + ")))))&&(E(X(" + p2 + ")))");
+        out.add("E(X(" + p1 + "))");
+        out.add("(!(E((!((!(E((!(" + p2 + "))U((!(true))&&(!(" + p2 + "))))))&&(!(E(G(!(" + p2 + ")))))))U((!((!(E((!(true))U((!(" + p1 + "))&&(!(true))))))&&(!(E(G(!(true)))))))&&(!((!(E((!(" + p2 + "))U((!(true))&&(!(" + p2 + "))))))&&(!(E(G(!(" + p2 + ")))))))))))&&(!(E(G(!((!(E((!(" + p2 + "))U((!(true))&&(!(" + p2 + "))))))&&(!(E(G(!(" + p2 + "))))))))))");
+        out.add("(true)&&(!(E(X(!(" + p1 + ")))))");
+        out.add("(true)&&(!(E(X(!((true)&&(true))))))");
+        out.add("E(X((!(E((!(" + p3 + "))U((!(" + p2 + "))&&(!(" + p3 + "))))))&&(!(E(G(!(" + p3 + ")))))))");
+        out.add("!(E(X(!(E(X(" + p3 + "))))))");
+        out.add("!(!(E(X(!(" + p2 + ")))))");
+        out.add("E(X((!(E((!(" + p3 + "))U((!(" + p3 + "))&&(!(" + p3 + "))))))&&(!(E(G(!(" + p3 + ")))))))");
+        out.add("!(!(E(X(!(true)))))");
+        out.add("(!(E((!(E((" + p2 + ")U(true))))U((!((!(E((!(" + p2 + "))U((!(" + p1 + "))&&(!(" + p2 + "))))))&&(!(E(G(!(" + p2 + ")))))))&&(!(E((" + p2 + ")U(true))))))))&&(!(E(G(!(E((" + p2 + ")U(true)))))))");
+
+
+        for (int i = 0; i < in.size(); i++) {
+            String form = getFormula(in.get(i)).existentialNormalForm().toString();
+            Assertions.assertEquals(form, out.get(i));
+        }
+
+    }
 
 }
--- src/test/PositiveNormalFormTest.java
+++ src/test/PositiveNormalFormTest.java
@@ -1,156 +1,196 @@
 package test;
 
-import static ctlform.PositiveTranslation.*;
-import static junit.framework.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
-
 import ctl.Formula;
 import ctl.Generator;
 import ctlform.PositiveNormalForm;
-import ctlform.PositiveTranslation;
 import ctlform.RandomFormula;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import parser.CTLLexer;
 import parser.CTLParser;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 class PositiveNormalFormTest {
 
+    public static final String p1 = "java.lang.error";
+    public static final String p2 = "java.lang.exception";
+    public static final String p3 = "java.lang.math";
 
 
+    private static Formula getFormula(String in) {
+        CharStream input = CharStreams.fromString(in);
+        CTLLexer lexer = new CTLLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CTLParser parser = new CTLParser(tokens);
+        ParseTree tree = parser.root();
+        Generator generator = new Generator();
+        return generator.visit(tree);
+    }
+
+    @Test
+    void testTranslatedDoubleNegation() {
+        String input1 = "!!True";
+        Formula formula1 = getFormula(input1);
+
+        Formula tester = PositiveNormalForm.translate(formula1);
+
+        Assertions.assertEquals("true", tester.toString());
+    }
 
 
-	private static Formula getFormula(String in) {
-		CharStream input = CharStreams.fromString(in);
-		CTLLexer lexer = new CTLLexer(input);
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		CTLParser parser = new CTLParser(tokens);
-		ParseTree tree = parser.root();
-		Generator generator = new Generator();
-		return generator.visit(tree);
-	}
+    @Test
+    void testFormulaUnchanged() {
 
-	@Test
-	void testTranslatedDoubleNegation(){
-		String input1 = "! ! True";
-		Formula formula1 = getFormula(input1);
+        for (int i = 0; i < 1000; i++) {
+            Formula generated = RandomFormula.UntranslatableRandomFormula(10);
 
-		Formula tester = PositiveNormalForm.translate(formula1);
+            Formula translated = generated.positiveNormalForm();
 
-		assertEquals("true", tester.toString());
-	}
+            Assertions.assertEquals(translated.toString(), generated.toString());
+        }
 
+        for (int i = 0; i < 1000; i++) {
+            Formula generated = RandomFormula.translatableENFRandomFormula(10);
 
-	@Test
-	void testFormulaUnchanged(){
+            Formula translated = generated.positiveNormalForm();
 
-		for(int i = 0; i < 1000; i++) {
-			Formula generated = RandomFormula.UntranslatableRandomFormula(10);
+            Assertions.assertEquals(translated.toString(), generated.toString());
+        }
 
-			Formula translated = generated.positiveNormalForm();
-
-			assertEquals(translated.toString(), generated.toString());
-		}
-
-		for(int i = 0; i < 1000; i++) {
-			Formula generated = RandomFormula.translatableENFRandomFormula(10);
-
-			Formula translated = generated.positiveNormalForm();
-
-			assertEquals(translated.toString(), generated.toString());
-		}
-
-	}
+    }
 
 
-	@Test
-	void testFormulaChanged(){
-		for(int i = 0; i < 1000; i++) {
-			Formula generated = RandomFormula.translatablePNFRandomFormula(10);
-			Formula translated = generated.positiveNormalForm();
-			boolean isSame = translated.toString().equals(generated.toString());
-			assertEquals(false,isSame);
-		}
-	}
+    @Test
+    void testFormulaChanged() {
+        for (int i = 0; i < 1000; i++) {
+            Formula generated = RandomFormula.translatablePNFRandomFormula(10);
+            Formula translated = generated.positiveNormalForm();
+            boolean isSame = translated.toString().equals(generated.toString());
+            assertFalse(isSame);
+        }
+    }
 
-	@Test
-	void testTranslatedNegatedTrue(){
-		String input1 = "! True";
-		Formula formula1 = getFormula(input1);
+    @Test
+    void testTranslatedNegatedTrue() {
+        String input1 = "! True";
+        Formula formula1 = getFormula(input1);
 
-		Formula tester = PositiveNormalForm.translate(formula1);
+        Formula tester = PositiveNormalForm.translate(formula1);
 
-		assertEquals("false", tester.toString());
-	}
+        Assertions.assertEquals("false", tester.toString());
+    }
 
-	@Test
-	void testTranslatedNegatedFalse(){
-		String input1 = "! false";
-		Formula formula1 = getFormula(input1);
+    @Test
+    void testTranslatedNegatedFalse() {
+        String input1 = "! false";
+        Formula formula1 = getFormula(input1);
 
-		Formula tester = PositiveNormalForm.translate(formula1);
+        Formula tester = PositiveNormalForm.translate(formula1);
 
-		assertEquals("true", tester.toString());
-	}
-
-
-	@Test
-	void testTranslatedNegatedAnd(){
-		String input1 = "!!!(!(!(!(Java.lang.Exception))) && !!!Java.lang.Runtime)";
-		Formula formula1 = getFormula(input1);
-
-		Formula test = PositiveNormalForm.translate(formula1);
-
-		String expectedOutput = "(Java.lang.Exception)||(Java.lang.Runtime)";
-		Formula expected = getFormula(expectedOutput);
-		assertEquals(expected, test);
-	}
-
-	@Test
-	void testTranslatedNegatedForAllNext(){
-		String input1 = "!A X True";
-		Formula formula1 = getFormula(input1);
-
-		Formula tester = PositiveNormalForm.translate(formula1);
-
-		System.out.println(tester.toString());
-	}
-
-	@Test
-	void testTranslatedNegatedExistsNext(){
-		String input1 = "!E X True";
-		Formula formula1 = getFormula(input1);
-
-		Formula tester = PositiveNormalForm.translate(formula1);
-
-		System.out.println(tester.toString());
-	}
-
-	//to do
-	@Test
-	void testTranslatedNegatedExistsUntil(){
-		String input1 = "!E(java.lang.Exception U java.lang.Exception)";
-		Formula formula1 = getFormula(input1);
-
-		Formula tester = PositiveNormalForm.translate(formula1);
-
-		System.out.println(tester.toString());
-	}
-
-	//to do
-	@Test
-	void testTranslatedNegatedForAllUntil(){
-		String input1 = "!A(java.lang.Exception U java.lang.Exception)";
-		Formula formula1 = getFormula(input1);
-		Formula tester = PositiveNormalForm.translate(formula1);
-		System.out.println(tester.toString());
-	}
+        Assertions.assertEquals("true", tester.toString());
+    }
 
 
+    @Test
+    void testTranslatedNegatedAnd() {
+        String input1 = "!!!(!(!(!(" + p1 + "))) && !!!" + p2 + ")";
+        Formula formula1 = getFormula(input1);
+
+        Formula test = PositiveNormalForm.translate(formula1);
+
+        String expectedOutput = "(" + p1 + ")||(" + p2 + ")";
+        Formula expected = getFormula(expectedOutput);
+        Assertions.assertEquals(expected, test);
+    }
+
+    @Test
+    void testTranslatedNegatedForAllNext() {
+        String input1 = "!A X True";
+        Formula formula1 = getFormula(input1);
+
+        Formula tester = PositiveNormalForm.translate(formula1);
+
+        Assertions.assertEquals("E(X(false))", tester.toString());
+    }
+
+    @Test
+    void testTranslatedNegatedExistsNext() {
+        String input1 = "!E X True";
+        Formula formula1 = getFormula(input1);
+
+        Formula tester = PositiveNormalForm.translate(formula1);
+
+        Assertions.assertEquals("A(X(false))", tester.toString());
+    }
+
+    //to do
+    @Test
+    void testTranslatedNegatedExistsUntil() {
+        String input1 = "!E(" + p1 + " U " + p1 + ")";
+        Formula formula1 = getFormula(input1);
+
+        Formula tester = PositiveNormalForm.translate(formula1);
+
+        Assertions.assertEquals("A(((" + p1 + ")&&(!(" + p1 + ")))W((!(" + p1 + "))&&(!(" + p1 + "))))", tester.toString());
+    }
+
+    //to do
+    @Test
+    void testTranslatedNegatedForAllUntil() {
+        String input1 = "!A(" + p1 + " U " + p1 + ")";
+        Formula formula1 = getFormula(input1);
+        Formula tester = PositiveNormalForm.translate(formula1);
+
+        Assertions.assertEquals("E(((" + p1 + ")&&(!(" + p1 + ")))W((!(" + p1 + "))&&(!(" + p1 + "))))", tester.toString());
+    }
+
+    @Test
+    void testMultipleNegationsAtomic() {
+
+        ArrayList<String> inputList = new ArrayList<String>();
+        inputList.add("!!!" + p1);
+        inputList.add("!!!!" + p1);
+        inputList.add("!!!!!" + p2);
+        inputList.add("!!!!! true");
+        inputList.add("!!!!!false");
+        inputList.add("!!!!!!!!" + p1);
+
+        ArrayList<String> outputList = new ArrayList<String>();
+        outputList.add("!(" + p1 + ")");
+        outputList.add("" + p1 + "");
+        outputList.add("!(" + p2 + ")");
+        outputList.add("false");
+        outputList.add("true");
+        outputList.add("" + p1 + "");
+
+        for (int i = 0; i < inputList.size(); i++) {
+            assertEquals(outputList.get(i), PositiveNormalForm.translate(getFormula(inputList.get(i))).toString());
+        }
+    }
+
+    @Test
+    void testRandomSamples() {
+        ArrayList<String> inputList = new ArrayList<String>();
+        inputList.add("!EX(" + p1 + "&&" + p2 + ")");
+        inputList.add("(E(" + p3 + " U " + p1 + ") && !true)");
+
+
+        ArrayList<String> outputList = new ArrayList<String>();
+        outputList.add("A(X((!(" + p1 + "))||(!(" + p2 + "))))");
+        outputList.add("(E((" + p3 + ")U(" + p1 + ")))&&(false)");
+
+        for (int i = 0; i < inputList.size(); i++) {
+            Formula formula = getFormula(inputList.get(i));
+            Formula out = PositiveNormalForm.translate(formula);
+            assertEquals(outputList.get(i), out.toString());
+        }
+    }
 }
